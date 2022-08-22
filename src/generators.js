35707e0410934e3f4e061dea1107533b8e6dd7dd
Timothy Erckert <timothyerckert@gmail.com>
2022-08-22 18:47:57 -0400

    working on remaining pages

---
+++ src/generators.js
@@ -0,0 +1,16 @@
+let defineH1 = (
+    id,         // ID of the h1 element
+    text        // Inner text of the h1 element
+    ) => {
+        hOne = document.createElement('h1')
+        hOne.id = id
+        hOne.innerText = text
+        return hOne
+    }
+
+
+
+
+
+
+
--- src/index.js
+++ src/index.js
@@ -11,23 +11,38 @@ const freshness = `Kelli's Roast Beef and Clams roasts our beef in-house and han
                   `fresh daily.` 
 
 const daysAndHours = [
+    {"day": "Sunday", "hours": "CLOSED"},
+    {"day": "Monday", "hours": "CLOSED"},
     {"day": "Tuesday", "hours": "3pm - 10pm"},
     {"day": "Wednesday", "hours": "3pm - 10pm"},
     {"day": "Thursday", "hours": "3pm - 10pm"},
     {"day": "Friday", "hours": "11am - 12am"},
     {"day": "Saturday", "hours": "11am - 12am"},
-    {"day": "Sunday", "hours": "CLOSED"},
-    {"day": "Monday", "hours": "CLOSED"},
 ]
-
-content.innerHTML = ""
 
 // Assign onclick properties
 const assignNavButtonOnclicks = () => {
-    homeNav.onclick = buildHome
-    console.log ("Need to create buildMenu function")
-    console.log ("Need to create buildContact function")
+	homeNav.onclick = buildHome
+	menuNav.onclick = buildMenu
+	contactNav.onclick = buildContact
+}	
+
+const clearContentOnPageLoad = () => {
+	content.innerHTML = ''
+	GenerateLogo()
 }
+
+const GenerateLogo = () => {
+    const hOne = document.createElement('h1')
+    hOne.id = 'logo'
+    hOne.innerText = `Kelli's Roast Beef Curtains and Clams`
+    content.append(hOne)
+}
+
+/***************************************************************************************
+ * Homepage generation
+***************************************************************************************/
+
 
 const updateNavButtons = newlySelectedButton => {
     // Get the currently selected button nav
@@ -38,73 +53,155 @@ const updateNavButtons = newlySelectedButton => {
     }
     // Remove not-selected class, add selected class
     newlySelectedButton.classList.replace('not-selected', 'selected')
+	clearContentOnPageLoad()
+}
+
+const generateHomecard = (
+	id,			// Element ID
+	titleText	// Element title Text
+) => {
+	// Create the list item and assign it's id and homecard.
+	const homecard = document.createElement('li')
+	homecard.id = id
+	homecard.setAttribute('class', 'homecard')
+
+	// Assign subheader to homecard element
+	const h3 = document.createElement('h3')
+	h3.innerText = titleText
+	// Attach homecard value
+	homecard.append(h3)
+	return homecard
+}
+
+const generateAndAppendParagraph = (
+	text, 		// Paragraph text
+	parent		// Paragraph parent
+) => {
+	paragraph = document.createElement('p')
+	paragraph.innerText = text
+	parent.append(paragraph)
+}
+
+const generateAndAppendHoursOfOperationTable = (
+	parent		// Table parent
+) => {
+	const tbody = document.createElement('tbody')
+	// Loop through array, generate, and append the row to the table.
+	daysAndHours.forEach( day => generateHoursOfOperationRows(day, tbody))
+
+	const table = document.createElement('table')
+	table.innerHTML = `<colgroup><col id="day"><col id="hours"></colgroup>`
+	table.append(tbody)
+
+	parent.append(table)
+}
+
+const generateHoursOfOperationRows = (
+    day,		// Element from dayAndHours array
+	tbody		// Table body that we are appending to
+) => {
+	const tr = document.createElement('tr')
+	let td = document.createElement('td')
+	td.innerText = day['day']
+	tr.append(td)
+	td = document.createElement('td')
+	td.innerText = day['hours']
+	tr.append(td)
+	tbody.append(tr)
 }
 
 const buildHome = () => {
     if (homeNav.classList.contains('selected')) return
     updateNavButtons(homeNav)               // Update the buttons values.
-    
 
-    // RESTAURANT NAME 
-    const logo = document.createElement('h1') // Restaurant title
-    logo.id = 'logo'
-    logo.innerText = `Kelli's Roast Beef and Clams`
-    
     // CONTENT BODY
     const mainBody = document.createElement('ul')
     
-    // Freshness guarantee
-    const weRule = document.createElement('li')
-    // Create freshness guarantee.
-    weRule.id = 'we-rule'
-    weRule.setAttribute('class', 'homecard')
-    let h3 = document.createElement('h3')
-    h3.innerText = "Our freshness guarantee..."
-    let p = document.createElement('p')
-    p.innerText = freshness
-    weRule.append(h3)
-    weRule.append(p)
-    mainBody.append(weRule)
+    // Create freshness guarantee homecard.
+    const freshnessGuarantee = generateHomecard('we-rule','Our freshness guarantee...')
+    generateAndAppendParagraph(freshness, freshnessGuarantee)
 
     // Create hours of operation element.
-    const hoursOfOp = document.createElement('li')
-    hoursOfOp.id = `hours`
-    hoursOfOp.setAttribute('class', 'homecard')
-    h3 = document.createElement('h3')
-    h3.innerText = "Restaurant Hours"
-    const table = document.createElement('table')
-    table.innerHTML = ` <colgroup><col id="day"><col id="hours"></colgroup>`
-    const tbody = document.createElement('tbody')
-    daysAndHours.forEach( day =>{
-        const tr = document.createElement('tr')
-        let td = document.createElement('td')
-        td.innerText = day['day']
-        tr.append(td)
-        td = document.createElement('td')
-        td.innerText = day['hours']
-        tr.append(td)
-        tbody.append(tr)
-    })
-    table.append(tbody)
-    hoursOfOp.append(h3)
-    hoursOfOp.append(table)
-    mainBody.append(hoursOfOp)
-
+    const hoursOfOp = generateHomecard('hours-table', `Restaurant Hours`)
+    generateAndAppendHoursOfOperationTable(hoursOfOp)
+	
     // Create address element.
-    const addy = document.createElement('li')
-    addy.id = `address`
-    addy.setAttribute('class', 'homecard')
-    h3 = document.createElement('h3')
-    h3.innerText = `Address`
-    p = document.createElement('p')
-    p.innerText = `101 213th Ave, Buffalo, NY 42069`
-    addy.append(h3)
-    addy.append(p)
-    mainBody.append(addy)
-
-    // Put it all together.
-    content.append(logo)
+    const address = generateHomecard(`address`, `Address`)
+	generateAndAppendParagraph(
+		`101 213th Ave, Buffalo, NY 42069`,
+		address
+	)
+    
+	// Attaching mainbody elements
+	mainBody.append(freshnessGuarantee)
+    mainBody.append(address)
+    mainBody.append(hoursOfOp)
+		
+    // Build site
     content.append(mainBody)
+}
+
+/***************************************************************************************
+ * Menu generation
+***************************************************************************************/
+  
+constMenuItems = [
+	{
+		"name":"Roast Beef on Weck",
+		"image":"./images/menu-items/beef-on-weck.jpg",
+		"credit":`<a href="http://1.bp.blogspot.com/_3sCgoK5pBp8/TC-ZrLzNPoI/AAAAAAAAA4w/n8KPul34_Ng/s1600/DSC_3123.jpg">Blogspot</a>`,
+		"description":"Our famous roast beef piled high on a house-made kimmelweck roll.",
+	},
+	// {
+	// 	"name":"Grilled Cheese",
+	// 	"image":"./images/menu-items/grilled-cheese.jpg",
+	// 	"credit":`Photo by <a href="https://unsplash.com/@llioangharad?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Llio Angharad</a> on <a href="https://unsplash.com/s/photos/roast-beef-sandwich?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>`,
+	// 	"description":"",
+	// },
+	{
+		"name":"Bacon Cheeseburger Sliders",
+		"image":"./images/menu-items/sliders.jpg",
+		"credit":`Photo by <a href="https://unsplash.com/@blitzer?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Niklas Rhöse</a> on <a href="https://unsplash.com/s/photos/roast-beef-sandwich?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>`,
+		"description":"Four bacon cheesburger sliders topped with ketchup, mustard, and pickled onions.",
+	},
+	{
+		"name":"Cheeseburger",
+		"image":"./images/menu-items/cheeseburger.jpg",
+		"credit":`Photo by <a href="https://unsplash.com/@loeees_?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Loes Klinker</a> on <a href="https://unsplash.com/s/photos/roast-beef-sandwich?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>`,
+		"description":"Two, seared one-third pound burger patties topped with gooey smoked cheddar, pickles, and pickled onions.",
+	},
+	{
+		"name":"Clams",
+		"image":"./images/menu-items/clams.jpg",
+		"credit":`Photo by <a href="https://unsplash.com/@adriensala?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Adrien Sala</a> on <a href="https://unsplash.com/s/photos/clams?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>`,
+		"description":"Fresh clams sauteed in a white wine, lemon-butter sauce.",
+	},
+	// Styling template for additional menu items
+	// {
+	// 	"name":"",
+	// 	"image":"./images/menu-items/",
+	// 	"credit":``,
+	// 	"description":"",
+	// },
+	
+]
+
+
+const buildMenu = () => {
+	if (menuNav.classList.contains('selected')) return
+    updateNavButtons(menuNav)
+	console.log ("Need to create buildMenu function")
+
+}
+
+/***************************************************************************************
+ * Contact generation
+***************************************************************************************/
+const buildContact = () => {
+	if (contactNav.classList.contains('selected')) return
+    updateNavButtons(contactNav) 
+	console.log ("Need to create buildContact function")
+
 }
 
 assignNavButtonOnclicks()
